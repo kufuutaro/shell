87cecbded15e2f21f173cd1883dcdf466b7f1316
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2026-05-09 01:40:38 +1000

    fix: modules not finding each other when installed

--- plugin/cmake/qml-module.cmake
+++ plugin/cmake/qml-module.cmake
@@ -25,11 +25,28 @@ function(qml_module arg_TARGET)
 
     message(STATUS "Created QML module: ${module_uri}")
 
+    # Modules can't link together properly if the backing target and plugin target
+    # are in the same dir (because diff modules can't access each other).
+    # So install backing targets to QMLDIR/.../lib/ instead.
+    string(REPLACE "/" ";" uri_parts "${module_target_path}")
+    list(GET uri_parts 0 top_level)
+    set(backing_lib_dir "${INSTALL_QMLDIR}/${top_level}/lib")
     set(module_dir "${INSTALL_QMLDIR}/${module_target_path}")
-    install(TARGETS ${arg_TARGET} LIBRARY DESTINATION "${module_dir}" RUNTIME DESTINATION "${module_dir}")
-    install(TARGETS "${module_plugin_target}" LIBRARY DESTINATION "${module_dir}" RUNTIME DESTINATION "${module_dir}")
+
+    install(TARGETS ${arg_TARGET}
+        LIBRARY DESTINATION "${backing_lib_dir}"
+        RUNTIME DESTINATION "${backing_lib_dir}"
+    )
+    install(TARGETS "${module_plugin_target}"
+        LIBRARY DESTINATION "${module_dir}"
+        RUNTIME DESTINATION "${module_dir}"
+    )
     install(FILES "${module_qmldir}" DESTINATION "${module_dir}")
     install(FILES "${module_typeinfo}" DESTINATION "${module_dir}")
 
     target_link_libraries(${arg_TARGET} PRIVATE caelestia-pch Qt::Core Qt::Qml ${arg_LIBRARIES})
+
+    # Add backing target dir to plugin rpath so it can find its backing target
+    file(RELATIVE_PATH plugin_to_lib "/${module_target_path}" "/${top_level}/lib")
+    set_property(TARGET ${module_plugin_target} APPEND PROPERTY INSTALL_RPATH "$ORIGIN/${plugin_to_lib}")
 endfunction()
